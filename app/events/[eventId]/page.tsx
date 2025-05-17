3bce45a14732e3d4ad901e2266598b47967701a0
jhyunwoo <jhyunwoo0228@gmail.com>
2025-05-17 22:13:36 +0900

    add articles

--- app/events/[eventId]/page.tsx
+++ app/events/[eventId]/page.tsx
@@ -2,6 +2,7 @@ import db from "@/db";
 import { articleTable, eventTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function EventPage({
   params,
@@ -27,16 +28,27 @@ export default async function EventPage({
         alt={event?.title || "Event Image"}
         width={600}
         height={400}
-        className="w-full h-full object-cover rounded-t-md"
+        className="w-full h-full object-cover"
       />
       <div className="p-4">
-        <div className="flex flex-col">
+        <div className="flex flex-col p-2">
           <h1 className="text-2xl font-bold">{event?.title}</h1>
           <p className="text-gray-500">{event?.description}</p>
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-4">
           {articles.map((article) => (
-            <div key={article.id}>{article.title}</div>
+            <Link
+              href={`/events/${eventId}/${article.id}`}
+              key={article.id}
+              className="flex flex-col gap-2 bg-white p-2 rounded-md"
+            >
+              <h3 className="text-lg font-semibold">{article.title}</h3>
+              <p className="text-gray-500 text-sm">{article.description}</p>
+              <div className="flex items-center gap-2 justify-start">
+                <p>{article.pressOrganization}</p>
+                <div>{article.createdAt.toLocaleDateString()}</div>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-neutral-50 pt-14">
+      <body className="bg-neutral-50 pt-16">
         <Header />
         {children}
       </body>
