3dceb5659e5a7c6a856b7b2d9aa57868255e4f30
jhyunwoo <jhyunwoo0228@gmail.com>
2025-05-18 12:14:59 +0900

    add logo on header

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
 import MenuBarButton from "./menu-bar-button";
 import MenuOptions from "./menu-options";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <>
       <div className="w-full p-4 flex justify-between items-center z-10 fixed top-0 left-0 bg-neutral-50 lg:px-24">
-        <Link href={"/"} className="text-xl font-bold">
-          UnbAIsly
-        </Link>
+        <div className={"flex items-center justify-center gap-2"}>
+          <Image
+            src={"/icon.png"}
+            alt={"UnbAIsly Logo"}
+            width={50}
+            height={50}
+          />
+          <Link href={"/"} className="text-xl font-bold">
+            UnbAIsly
+          </Link>
+        </div>
         <MenuBarButton />
       </div>
       <MenuOptions />
